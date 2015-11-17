178019cf6de2535f523fc904e6811150fea0c64d
awesomegamer <avay_raul@hotmail.com>
2015-11-16 23:42:50 -0600

    multilayer network prototype

--- src/bigann/core.clj
+++ src/bigann/core.clj
@@ -56,7 +56,7 @@
 		ch))
 
 (defn parser [^String line]
-	(let [[x1 x2 _ x4 x5 _ x7 x8 x9 x10 x11 xHot x13 x14 x15] 
+	(let [[x1 x2 _ _ x5 _ x7 x8 x9 x10 x11 _ x13 x14 _] 
 					(-> 
 						line
 						(str/lower-case)
@@ -68,7 +68,7 @@
 						(if (= 0.0 (Float. item))
 							0.0001
 							(Float. item)))
-			[x1 x2 x4 x5 x7 x8 x9 x10 x11 x13 x14 x15 xHot])))
+			[x1 x2 x5 x7 x8 x9 x10 x11 x14 x13])))
 
 (defn async-fetch
 	[]
@@ -81,7 +81,7 @@
 										(->>
 											lines
 											(map parser)
-											(filter #(not= (last %) 0.0001))
+											; (filter #(not= (last %) 0.0001))
 											(shuffle))]
 							(doseq [line rows]
 								(>! ch line)))
@@ -119,14 +119,16 @@
 
 (def td-normals (future (fetch-normalizer)))
 
-(def i-layer-sz "Input layer size" 12)
-(def h-layer-sz "Input layer size" 80)
+(def i-layer-sz "Input layer size" 9)
+(def h-layer-sz "Hidden 1 layer size" 24)
+(def h2-layer-sz "Hidden 2 layer size" 24)
 (def o-layer-sz "Output layer size" 1)
-(def iteration "Total iteration for training" 250)
+(def iteration "Total iteration for training" 200)
 (def rate "Rate of change of weights" 0.9)
 
 (def w1 (future (gen-matrix i-layer-sz h-layer-sz)))
-(def w2 (future (gen-matrix h-layer-sz o-layer-sz)))
+(def w2 (future (gen-matrix h-layer-sz h2-layer-sz)))
+(def w3 (future (gen-matrix h2-layer-sz o-layer-sz)))
 
 (defn expn
 	"Computes the exponential of a matrix"
@@ -163,63 +165,73 @@
 
 (defn feed
 	"Feed forwards a row with given weight and returns yHat"
-	[row w1 w2]
+	[row w1 w2 w3]
 	(let [x [(pop row)]
 				y [[(peek row)]]
 				z2 (dot x w1)
 				a2 (sigmoid z2)
 				z3 (dot a2 w2)
-				yHat (sigmoid z3)]
+				a3 (sigmoid z3)
+				z4 (dot a3 w3)
+				yHat (sigmoid z4)]
 		yHat))
 
 (defn feed-forward
-	[row w1 w2]
+	[row w1 w2 w3]
 	(let [x [(pop row)]
 				y [[(peek row)]]
 				z2 (dot x w1)
 				a2 (sigmoid z2)
 				z3 (dot a2 w2)
-				yHat (sigmoid z3)]
+				a3 (sigmoid z3)
+				z4 (dot a3 w3)
+				yHat (sigmoid z4)]
 
-		(let [delta3 (Mat/* (-ve (Mat/- y yHat)) (sigmoid-prime z3))
+		(let [delta4 (Mat/* (-ve (Mat/- y yHat)) (sigmoid-prime z4))
+					djdw3 (dot (transpose a3) delta4)
+					delta3 (Mat/* (dot delta4 (transpose w3)) (sigmoid-prime z3))
 					djdw2 (dot (transpose a2) delta3)
 					delta2 (Mat/* (dot delta3 (transpose w2)) (sigmoid-prime z2))
 					djdw1 (dot (transpose x) delta2)
+
+					deltaW3 (Mat/- w3 (Mat/* rate djdw3))
 					deltaW2 (Mat/- w2 (Mat/* rate djdw2))
 					deltaW1 (Mat/- w1 (Mat/* rate djdw1))
 					[[ error ]] (Mat/- y yHat)]
 
-			(let [[[ error-2 ]] (Mat/- y (feed row deltaW1 deltaW2))]
+			(let [[[ error-2 ]] (Mat/- y (feed row deltaW1 deltaW2 deltaW3))]
 				(cond
 					(< error 0)
 						(if (< error-2 error)
-							{:w1 w1 :w2 w2} 
-							{:w1 deltaW1 :w2 deltaW2})
+							{:w1 w1 :w2 w2 :w3 w3} 
+							{:w1 deltaW1 :w2 deltaW2 :w3 deltaW3})
 					:else
 						(if (< error-2 error) 
-							{:w1 deltaW1 :w2 deltaW2} 
-							{:w1 w1 :w2 w2}))))))
+							{:w1 deltaW1 :w2 deltaW2 :w3 deltaW3} 
+							{:w1 w1 :w2 w2 :w3 w3}))))))
 
 (defn propagate
-	[wh1 wh2]
+	[wh1 wh2 wh3]
 	(read-data ch
-		(loop [acc wh1 acc2 wh2]
+		(loop [acc wh1 acc2 wh2 acc3 wh3]
 			(if-let [line (<!! ch)]
-				(let [ret (feed-forward (vec (@td-normals line)) acc acc2)]
-					(recur (:w1 ret) (:w2 ret)))
-				{:w1 acc :w2 acc2}))))
+				(let [ret (feed-forward (vec (@td-normals line)) acc acc2 acc3)]
+					(recur (:w1 ret) (:w2 ret) (:w3 ret)))
+				{:w1 acc :w2 acc2 :w3 acc3}))))
 
 (def weight
-	(loop [n 0 acc @w1 acc2 @w2]
+	(loop [n 0 acc @w1 acc2 @w2 acc3 @w3]
 		(if (< n iteration)
-			(let [ret (propagate acc acc2)]
-				(recur (inc n) (:w1 ret) (:w2 ret)))
-			{:w1 acc :w2 acc2})))
+			(let [ret (propagate acc acc2 acc3)]
+				(recur (inc n) (:w1 ret) (:w2 ret) (:w3 ret)))
+			{:w1 acc :w2 acc2 :w3 acc3})))
 
 (forward line
 	(let [normalized (vec (@td-normals line))
-				yHat (feed normalized (:w1 weight) (:w2 weight))]
-		(println yHat (last normalized))))
+				yHat (feed normalized (:w1 weight) (:w2 weight) (:w3 weight))]
+		(pm yHat)
+		(pm (last normalized))))
+
 
 (defn -main
   [& args]
